7741f8203ce95457d0d8664b08b220d2a4562e97
jnsltk <janos.litkei@gmail.com>
2022-10-09 14:34:01 +0200

    Implement tones

--- script.js
+++ script.js
@@ -3,8 +3,39 @@ const outputField = document.querySelector('#output-text');
 
 const convertBtn = document.querySelector('#convert');
 
+// const pinyinTones = ["1", "2", "3", "4", "5"];
+const zhuyinTones = ["", "ˊ", "ˇ", "ˋ", "˙"]
+
 function btnPress() {
-    outputField.value = inputField.value;
+    splitInputToWords(inputField.value);
+    outputField.value = convertSentence(inputField.value);
+}
+
+function splitInputToWords(inputString) {
+    return inputString.split(" ");
+}
+
+function convertSentence(wordsString) {
+    let wordsArray = splitInputToWords(wordsString);
+    let zhuyinSentence = "";
+    for (let i = 0; i < wordsArray.length; i++) {
+        zhuyinSentence += convert(wordsArray[i]) + " ";        
+    }
+    return zhuyinSentence;
+}
+
+function convert(word) {
+    let zhuyinTone = getTone(word);
+    let initial = getInitial(word);
+    return word.replace(/.$/, zhuyinTone);
+}
+
+function getTone(word) {
+    return zhuyinTones[parseInt(word.slice(-1)) - 1];
+}
+
+function getInitial(word) {
+
 }
 
 convertBtn.addEventListener('click', btnPress);
